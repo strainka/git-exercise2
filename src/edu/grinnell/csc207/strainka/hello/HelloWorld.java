ac7e26bd05852b27667fcdffbf4d6c1855f251ef
strainka <strainka@grinnell.edu>
2014-01-23 16:41:06 -0600

    Update HelloWorld.java

--- src/edu/grinnell/csc207/strainka/hello/HelloWorld.java
+++ src/edu/grinnell/csc207/strainka/hello/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 	
 	public static void main(String[] args) {
 		
-		System.out.println("Hello, GitHub!");
+		System.out.println("Hello, GitHub! How are you?");
 		
 	} // main(String[])
 	
